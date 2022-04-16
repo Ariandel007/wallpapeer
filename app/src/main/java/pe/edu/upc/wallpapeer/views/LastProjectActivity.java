45b758c577923a440b78144a02335d3268bb6efb
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2022-04-16 18:15:30 -0500

    bind ImageView

--- app/src/main/java/pe/edu/upc/wallpapeer/views/LastProjectActivity.java
+++ app/src/main/java/pe/edu/upc/wallpapeer/views/LastProjectActivity.java
@@ -5,23 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 //
 //import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 //import android.view.View;
 //import android.widget.Button;
 //import android.widget.ImageView;
 
 import pe.edu.upc.wallpapeer.R;
+import pe.edu.upc.wallpapeer.viewmodels.LastProjectViewModel;
 //import pe.edu.upc.wallpapeer.viewmodels.LastProjectViewModel;
 
 public class LastProjectActivity extends AppCompatActivity {
 
-//    private LastProjectViewModel model;
-//    Button btnQr = findViewById(R.id.btnQrCode);
-//    ImageView imgQr = findViewById(R.id.ivQr);
+    private LastProjectViewModel model;
+    Button btnQr;
+    ImageView imgQr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_last_project);
+
+        btnQr = findViewById(R.id.btnQrCode);
+        imgQr = findViewById(R.id.ivQr);
 //        model = ViewModelProviders.of(this).get(LastProjectViewModel.class);
 
 //        btnQr.setOnClickListener(new View.OnClickListener() {
