74c5964e6a295ef4a391beaf0f4f4115706ae8fa
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2022-05-11 03:17:05 -0500

    standar selected otpion

--- app/src/main/java/pe/edu/upc/wallpapeer/utils/PaletteState.java
+++ app/src/main/java/pe/edu/upc/wallpapeer/utils/PaletteState.java
@@ -3,8 +3,8 @@ package pe.edu.upc.wallpapeer.utils;
 public class PaletteState {
     private static PaletteState INSTANCE;
 
-    private Integer selectedOption = 5;
-    private int subOption = 1;
+    private Integer selectedOption = 0;
+    private int subOption = -1;
     private String textToPrint;
 
     public static PaletteState getINSTANCE() {
