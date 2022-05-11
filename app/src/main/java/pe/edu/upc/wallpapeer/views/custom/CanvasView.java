d90796923ff7bffc9db6ad5a9ca8d009c3e48471
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2022-05-11 01:50:36 -0500

    gesture moving

--- app/src/main/java/pe/edu/upc/wallpapeer/views/custom/CanvasView.java
+++ app/src/main/java/pe/edu/upc/wallpapeer/views/custom/CanvasView.java
@@ -1,11 +1,13 @@
 package pe.edu.upc.wallpapeer.views.custom;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -33,6 +35,8 @@ import pe.edu.upc.wallpapeer.utils.AppDatabase;
 import pe.edu.upc.wallpapeer.utils.CodeEvent;
 import pe.edu.upc.wallpapeer.utils.JsonConverter;
 import pe.edu.upc.wallpapeer.utils.MyLastPinch;
+import pe.edu.upc.wallpapeer.utils.PaletteOption;
+import pe.edu.upc.wallpapeer.utils.PaletteState;
 import pe.edu.upc.wallpapeer.viewmodels.ConnectionPeerToPeerViewModel;
 
 public class CanvasView  extends View {
@@ -76,12 +80,64 @@ public class CanvasView  extends View {
                 canvas.drawCircle(element.getPosxElement(),element.getPosyElement(),element.getWidthElement(), mPaint);
             }
 
+            if(element.getTypeElement().equals("square_figure")) {
+                element.setPosyElement(element.getPosyElement() - currentCanvaEntity.getPosY());
+                element.setPosxElement(element.getPosxElement() - currentCanvaEntity.getPosX());
+                mPaint = new Paint();
+                mPaint.setStyle(Paint.Style.FILL);
+                mPaint.setColor(Color.BLUE);
+                drawSquare(element.getPosxElement(), element.getPosyElement(), element.getWidthElement(), element.getHeightElement(),canvas, mPaint);
+            }
+
+            if(element.getTypeElement().equals("triangle_figure")) {
+                element.setPosyElement(element.getPosyElement() - currentCanvaEntity.getPosY());
+                element.setPosxElement(element.getPosxElement() - currentCanvaEntity.getPosX());
+                mPaint = new Paint();
+                mPaint.setStyle(Paint.Style.FILL);
+                mPaint.setColor(Color.BLUE);
+                drawTriangle(element.getPosxElement(), element.getPosyElement(), element.getWidthElement(), canvas, mPaint);
+            }
         }
 
     }
 
+    public void drawSquare(float x, float y, float width, float heigth, Canvas canvas, Paint mPaint) {
+        double squareSideHalf = 1 / Math.sqrt(2);
+        Rect rectangle = new Rect((int) (x - (squareSideHalf * width)), (int) (y - (squareSideHalf * heigth)), (int) (x + (squareSideHalf * width)), (int) (y + ((squareSideHalf * heigth))));
+        canvas.drawRect(rectangle, mPaint);
+    }
+
+    public void drawTriangle(float x, float y, float width, Canvas canvas, Paint mPaint) {
+        int halfWidth = (int) (width / 2);
+        Path path = new Path();
+        path.moveTo(x, y - halfWidth); // Top
+        path.lineTo(x - halfWidth, y + halfWidth); // Bottom left
+        path.lineTo(x + halfWidth, y + halfWidth); // Bottom right
+        path.lineTo(x, y - halfWidth); // Back to Top
+        path.close();
+        canvas.drawPath(path, mPaint);
+    }
+
     public void setBackgroundFill(@ColorInt int backgroundFill){
         this.backgroundFill = backgroundFill;
+    }
+
+    public boolean checkIfPointIsInside(int x1, int y1, int x2, int y2, int x, int y) {
+        return x >= x1 && x <= x2 && y >= y1 && y <= y2;
+    }
+
+    public List<Element> filterElementsInsidePoint(float posX, float posY, List<Element> allElements) {
+        List<Element> fileteredElements =new ArrayList<>();
+        for (Element element : allElements) {
+            int yPosEle = (int) (element.getPosyElement()) - (int) element.getHeightElement()/2;
+            int xPosEle = (int) (element.getPosxElement()) - (int) element.getWidthElement()/2;
+            int yPosEleFinal = (int) (element.getPosyElement()) + (int) element.getHeightElement()/2;
+            int xPosEleFinal = (int) (element.getPosxElement()) + (int) element.getWidthElement()/2;
+            if(checkIfPointIsInside(xPosEle, yPosEle, xPosEleFinal, yPosEleFinal,(int) posX, (int)posY)) {
+                fileteredElements.add(element);
+            }
+        }
+        return fileteredElements;
     }
 
     @Override
@@ -229,55 +285,95 @@ public class CanvasView  extends View {
     class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
         private static final String DEBUG_TAG = "Gestures";
 
+        List<Element> listFiltered;
+        Element newElement;
+        @SuppressLint("CheckResult")
         @Override
         public boolean onDown(MotionEvent event) {
             if(!isPinchLocked) {
                 return true;
             }
+            listFiltered = new ArrayList<>();
             Log.d(DEBUG_TAG,"onDown: " + event.toString());
             posX = event.getX() + getCurrentCanvaEntity().getPosX();
             posY = event.getY() + getCurrentCanvaEntity().getPosY();
+            listFiltered = filterElementsInsidePoint(posX, posY, elementListCanvas);
 
-        switch (event.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                //SAVE AN ELEMENT
-                Element newElement = new Element();
-                newElement.setId(UUID.randomUUID().toString());
-                newElement.setTypeElement("circle_figure");
-                newElement.setRotation(0);
-                newElement.setzIndex(0);
-                newElement.setHeightElement(30);
-                newElement.setWidthElement(30);
-                newElement.setPosxElement(posX);
-                newElement.setPosyElement(posY);
-                newElement.setDateCreation(new Date());
-                newElement.setId_project(currentProjectEntity.id);
+            Log.i("Size listFiltered", String.valueOf(listFiltered.size()));
 
+            newElement = null;
+            switch (event.getAction()){
+                case MotionEvent.ACTION_DOWN:
+
+                    if(listFiltered.size() == 0) {
+                        if(PaletteOption.SHAPES_OPTION == PaletteState.getInstance().getSelectedOption()
+                                && PaletteOption.SHAPES_OPTION_CIRCLE== PaletteState.getInstance().getSubOption()) {
+                            newElement = new Element();
+                            newElement.setId(UUID.randomUUID().toString());
+                            newElement.setTypeElement("circle_figure");
+                            newElement.setRotation(0);
+                            newElement.setzIndex(0);
+                            newElement.setHeightElement(200);
+                            newElement.setWidthElement(200);
+                            newElement.setPosxElement(posX);
+                            newElement.setPosyElement(posY);
+                            newElement.setDateCreation(new Date());
+                            newElement.setId_project(currentProjectEntity.id);
+                        }
+
+                        if(PaletteOption.SHAPES_OPTION == PaletteState.getInstance().getSelectedOption()
+                                && PaletteOption.SHAPES_OPTION_SQUARE== PaletteState.getInstance().getSubOption()) {
+                            newElement = new Element();
+                            newElement.setId(UUID.randomUUID().toString());
+                            newElement.setTypeElement("square_figure");
+                            newElement.setRotation(0);
+                            newElement.setzIndex(0);
+                            newElement.setHeightElement(200);
+                            newElement.setWidthElement(200);
+                            newElement.setPosxElement(posX);
+                            newElement.setPosyElement(posY);
+                            newElement.setDateCreation(new Date());
+                            newElement.setId_project(currentProjectEntity.id);
+                        }
+
+                        if(PaletteOption.SHAPES_OPTION == PaletteState.getInstance().getSelectedOption()
+                                && PaletteOption.SHAPES_OPTION_TRIANGLE== PaletteState.getInstance().getSubOption()) {
+                            newElement = new Element();
+                            newElement.setId(UUID.randomUUID().toString());
+                            newElement.setTypeElement("triangle_figure");
+                            newElement.setRotation(0);
+                            newElement.setzIndex(0);
+                            newElement.setHeightElement(200);
+                            newElement.setWidthElement(200);
+                            newElement.setPosxElement(posX);
+                            newElement.setPosyElement(posY);
+                            newElement.setDateCreation(new Date());
+                            newElement.setId_project(currentProjectEntity.id);
+                        }
+                    }
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    break;
+                    /*respecto a un area*/
+                case MotionEvent.ACTION_UP:
+                    break;
+            }
+            //ACA SE GUARDA, ENVIA y LLAMA AL OBSERVABLE PARA PINTAR EL ELEMENTO
+            if(newElement != null) {
                 ///Informamos a los dispositivos el cambio
                 getModel().sendMessage(JsonConverter.getGson().toJson(new NewElementInserted(CodeEvent.INSERT_NEW_ELEMENT, newElement)));
-
                 AppDatabase.getInstance().elementDAO().insert(newElement).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread()).subscribe(() -> {
+                        .observeOn(AndroidSchedulers.mainThread()).subscribe(() -> {
                     Log.i("Se creo","Se creo con exito");
                 }, throwable -> {
                     Log.e("Error","Error al crear");
                 });
-                //Se termino de crear
-
-                //SET PATH
-                mPath = new Path();
-                mPath.moveTo(posX,posY);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                break;
-                /*respecto a un area*/
-            case MotionEvent.ACTION_UP:
-                break;
-        }
-
+            }
+            //Se termino de crear
             return true;
         }
 
+        @SuppressLint("CheckResult")
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2,
                                float velocityX, float velocityY) {
@@ -287,15 +383,37 @@ public class CanvasView  extends View {
             try{
                 if(!isPinchLocked) {
                     sendCoordsToPinch(xDiff, yDiff, e2, 100, velocityX, velocityY, 100);
+                } else {
+
+                    float xMoved = e2.getX() + getCurrentCanvaEntity().getPosX();
+                    float yMoved = e2.getY() + getCurrentCanvaEntity().getPosY();
+                    if (listFiltered.size() > 0){
+                        newElement = listFiltered.get(listFiltered.size()-1);
+                        newElement.setPosxElement(xMoved);
+                        newElement.setPosyElement(yMoved);
+
+                        if(newElement != null) {
+                            ///Informamos a los dispositivos el cambio
+                            getModel().sendMessage(JsonConverter.getGson().toJson(new NewElementInserted(CodeEvent.INSERT_NEW_ELEMENT, newElement)));
+                            AppDatabase.getInstance().elementDAO().insert(newElement).subscribeOn(Schedulers.io())
+                                    .observeOn(AndroidSchedulers.mainThread()).subscribe(() -> {
+                                Log.i("Se creo","Se creo con exito");
+                            }, throwable -> {
+                                Log.e("Error","Error al crear");
+                            });
+                        }
+                    }
                 }
                 return true;
             }catch (Exception e){
                 e.printStackTrace();
             }
-            return false;
+//            return false;
 
-//            return true;
+            return true;
         }
+
+
     }
 
 }
