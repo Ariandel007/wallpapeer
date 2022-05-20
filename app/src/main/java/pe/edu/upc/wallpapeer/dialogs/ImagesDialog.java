83536d855c99656e5534c7774cdcf079b29da4ac
Deiroo <renatoballon.rb@gmail.com>
2022-05-19 21:23:23 -0500

    adding images dialog

---
+++ app/src/main/java/pe/edu/upc/wallpapeer/dialogs/ImagesDialog.java
@@ -0,0 +1,49 @@
+package pe.edu.upc.wallpapeer.dialogs;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import pe.edu.upc.wallpapeer.R;
+
+public class ImagesDialog extends AppCompatDialogFragment {
+
+    private ImagesDialogListener listener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle("Seleccione su opción")
+                .setItems(R.array.shapes_array, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // The 'which' argument contains the index position
+                        // of the selected item
+                        listener.applyImageOption(which);
+                    }
+                });
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (ImagesDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString());
+        }
+    }
+
+    public interface ImagesDialogListener{
+        void applyImageOption(int option);
+    }
+}
